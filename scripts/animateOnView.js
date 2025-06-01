ed0f29722176d0a1bf7b3bcecf650f52a81665c9
MatGAAM <matheus-gustavo02@outlook.com>
2025-06-01 13:55:34 -0300

    criando animações para os titulos com js

---
+++ scripts/animateOnView.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    // console.log("observer", observer);
+    (entries, observer) => {
+      console.log("entries", entries);
+      console.log("entries observer", observer);
+      entries.forEach((entry) => {
+        console.log("entry", entry);
+        console.log("entry.isIntersecting", entry.isIntersecting);
+        if (entry.isIntersecting) {
+          entry.target.firstElementChild.classList.add("slider-fade-in");
+          // observer.unobserve(entry.target);
+        } else {
+          entry.target.firstElementChild.classList.remove("slider-fade-in");
+        }
+        console.log("entry.target.classList", entry.target.classList);
+      });
+    },
+    {
+      threshold: 0.4,
+    }
+  );
+
+  const elements = document.querySelectorAll(".observe-title");
+  elements.forEach((el) => observer.observe(el));
+
+  console.log("IntersectionObserver ativo");
+});
