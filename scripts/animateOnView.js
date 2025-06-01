41339ad9b014fc594a05b24fa5a95501cc008cf5
MatGAAM <matheus-gustavo02@outlook.com>
2025-06-01 15:20:05 -0300

    ajustes finais de fadeIn

--- scripts/animateOnView.js
+++ scripts/animateOnView.js
@@ -2,18 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(
     // console.log("observer", observer);
     (entries, observer) => {
-      console.log("entries", entries);
-      console.log("entries observer", observer);
       entries.forEach((entry) => {
-        console.log("entry", entry);
-        console.log("entry.isIntersecting", entry.isIntersecting);
         if (entry.isIntersecting) {
           entry.target.classList.add("slider-fade-in");
-          // observer.unobserve(entry.target);
         } else {
           entry.target.classList.remove("slider-fade-in");
         }
-        console.log("entry.target.classList", entry.target.classList);
       });
     },
     {
